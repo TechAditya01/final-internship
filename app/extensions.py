f55dbd082a22051ac2f2984fc1bb664ff1d97158
Aditya Kumar <166497239+TechAditya01@users.noreply.github.com>
2025-11-26 23:48:33 +0530

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 migrate = Migrate()
