39199a140488d3b9396176b8129b30bda02edfbf
Aditya Kumar <166497239+TechAditya01@users.noreply.github.com>
2025-11-27 14:57:40 +0530

    Update oauth.py

--- app/oauth.py
+++ app/oauth.py
@@ -7,7 +7,7 @@ LOG = logging.getLogger(__name__)
 CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 # Make sure your Render redirect URI matches this (or set OAUTH_REDIRECT_URI env var)
-REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI", "http://127.0.0.1:5000/oauth2callback")
+REDIRECT_URI = os.getenv("redirect_uri = os.getenv("GOOGLE_REDIRECT_URI")
 
 SCOPES = "openid email profile"
 
